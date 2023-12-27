d4b5d52177d868ee6ee3c0768df81e0339354a25
Danjiil01 <danikpendo20128@gmail.com>
2023-12-27 12:50:39 +0300

    Second commit
    Added interface with constants

---
+++ src/com/teachmeskills/lesson16/homework/task1/constant/IPassportNumber.java
@@ -0,0 +1,6 @@
+package com.teachmeskills.lesson16.homework.task1.constant;
+
+public interface IPassportNumber {
+    String PASSPORT_CLIENT_1 = "MP93829489";
+    String PASSPORT_CLIENT_2 = "ER91040124";
+}
